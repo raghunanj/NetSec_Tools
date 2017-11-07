855298e1fa6d6ed2dd7f8f77386a7bbe1101bf01
raghunanj <kothvariraghu@gmail.com>
2017-11-07 18:46:04 -0500

    HW3-Code to read the file

--- Assignment3/pbproxy.c
+++ Assignment3/pbproxy.c
@@ -3,5 +3,30 @@
 #include <string.h>
 #include <stdio.h>
 #include <stdlib.h>
+
+
+char* read_file(char* filename){
+	char* buf = 0;
+	unsigned long len;
+	FILE *fd = fopen(filename, "rb");
+
+	if (fd){
+		fseek(fd, 0 , SEEK_END);
+		len = ftell(fd);
+		fseek(fd, 0, SEEK_SET);
+		buf = malloc(len);
+		if(buf){
+			fread(buf, 1, len,fd);
+		fclose(fd);
+		} 
+	}
+	else 
+		return 0;
+
+	return buf;
+}
+
 int main(int argc, char *argv[]) {
 }
+
+
